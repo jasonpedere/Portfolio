27e1ee69cc659802c93deda98cbc1c3002e82fb3
jasonpedere <jasonpedere@gmail.com>
2026-01-28 11:41:48 +0800

    Revert to standard navbar setup

--- App.tsx
+++ App.tsx
@@ -105,7 +105,7 @@ const App: React.FC = () => {
       {/* Hide navbar on manage page for cleaner admin look */}
       {currentPath !== '#/manage' && <Navbar />}
       
-      <div className="pt-16">
+      <div>
         {renderContent()}
       </div>
 
